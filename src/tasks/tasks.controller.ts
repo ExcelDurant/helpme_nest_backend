9b52afa14b20362f3018eff7cb8452566028f101
ExcelDurant <gnopaexcel@yahoo.fr>
2021-10-28 04:22:10 +0100

    protecting creating task route

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService:TasksService) {}
-
+    
+    @UseGuards(JwtAuthGuard)
     @Post('create')
-    async signup(@Body() body) {
+    async signup(@Body() body, @Request() req) {
+        body.created_by = req.user._id;
         return this.tasksService.createTask(body);
         // return body;
     }
